064b033a139c706270f0cdf2391d919e482b523c
Keitty <116025068+Kei-Degregorio-Davinci@users.noreply.github.com>
2024-01-16 21:07:27 -0300

    Initial commit

---
+++ app/src/main/java/com/example/mkdivapp/ActividadActivity.java
@@ -0,0 +1,14 @@
+package com.example.mkdivapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ActividadActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_actividad);
+    }
+}
---
+++ app/src/main/java/com/example/mkdivapp/MenuUsuariosActivity.java
@@ -0,0 +1,14 @@
+package com.example.mkdivapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MenuUsuariosActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu_usuarios);
+    }
+}
